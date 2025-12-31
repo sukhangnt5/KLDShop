720aa57b656ae27e701bb56dff2e16938fa2df5a
Su Khang <sukhang@example.com>
2025-12-31 11:33:39 +0700

    fix: update remaining DateTime.Now in VNPayService

--- Services/PaymentGateway/VNPayService.cs
+++ Services/PaymentGateway/VNPayService.cs
@@ -45,14 +45,14 @@ namespace KLDShop.Services.PaymentGateway
                     };
                 }
 
-                var tick = DateTime.Now.Ticks.ToString();
+                var tick = DateTime.UtcNow.Ticks.ToString();
                 var vnpay = new VnPayLibrary();
 
                 vnpay.AddRequestData("vnp_Version", "2.1.0");
                 vnpay.AddRequestData("vnp_Command", "pay");
                 vnpay.AddRequestData("vnp_TmnCode", _vnpayTmnCode);
                 vnpay.AddRequestData("vnp_Amount", ((long)(request.Amount * 100)).ToString());
-                vnpay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss"));
+                vnpay.AddRequestData("vnp_CreateDate", DateTime.UtcNow.ToString("yyyyMMddHHmmss"));
                 vnpay.AddRequestData("vnp_CurrCode", "VND");
                 vnpay.AddRequestData("vnp_IpAddr", GetClientIpAddress());
                 vnpay.AddRequestData("vnp_Locale", "vn");
